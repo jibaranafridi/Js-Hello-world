8b0fef7af78c4685dd131518c45a848ba09ae91c
jibrankhan <jibrakha088@gmail.com>
2024-03-10 15:14:08 +0500

    Assigemnt 10 completed

---
+++ app.js
@@ -0,0 +1,4 @@
+document.write("<h1>Hello  <span class='span'>Javascript</span> Planet</h1>");
+document.write("I am the <b>master</b> of my own fate.</br>");
+document.write("I am the <b>champion</b> of my own soul.</br>");
+document.write("I am the <b>superhero</b> of my own life.</br>");
